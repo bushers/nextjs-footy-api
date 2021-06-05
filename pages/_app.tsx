b880567b0dc2fb6717d0d412fb713fae8f93d3e6
nickVerve <nick@vervesearch.com>
2021-06-05 21:40:50 +0100

    Inital commit, pull from footy api

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Component {...pageProps} />
+        </QueryClientProvider>
+    );
+}
+export default MyApp;
